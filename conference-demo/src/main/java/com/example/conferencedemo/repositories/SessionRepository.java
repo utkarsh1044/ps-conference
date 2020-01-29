b5f73eb5dbeb3a359fb9b71ac55c7092dba18c9d
Utkarsh Gupta <utkarsh.gupta@proptiger.com>
2020-01-29 23:29:23 +0530

    Removed test directory

--- conference-demo/src/main/java/com/example/conferencedemo/repositories/SessionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.conferencedemo.repositories;
-
-import com.example.conferencedemo.models.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session,Long> {
-}
--- conference-demo/src/main/java/com/example/conferencedemo/repositories/SpeakerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.conferencedemo.repositories;
-
-import com.example.conferencedemo.models.Speaker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpeakerRepository extends JpaRepository<Speaker,Long> {
-}
